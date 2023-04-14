22df9d62ace21ac195fff77ddbd29ba2c98f7255
Victor Vicente <vicente.victor1507@gmail.com>
2023-04-13 23:05:47 -0300

    end upload

--- js/script.js
+++ js/script.js
@@ -12,12 +12,22 @@ const jump = () => {
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
+    const octodexPosition = +window.getComputedStyle(octodex).bottom.replace('px', '');
+
+    console.log(octodexPosition);
     
-    if(pipePosition <= 134){
+    if(pipePosition <= 135 && pipePosition > 0 && octodexPosition < 80){
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
+        octodex.style.animation = 'none';
+        octodex.style.bottom = `${octodexPosition}px`;
+
+        octodex.src = './images/game-over.png';
+        octodex.style.width = '120px'
+
+        clearInterval(loop);
     }
 
 },10)
