8dc79597d3084d712bb6d022b6832f17757d8cad
Victor Vicente <vicente.victor1507@gmail.com>
2023-04-15 12:23:06 -0300

    upgrade restart

--- js/script.js
+++ js/script.js
@@ -8,50 +8,70 @@ let jogar = false;
 
 if(jogar == false){
 
-    octodex.style.animation = 'none'
     pipe.style.animation = 'none';
     pipe.style.right = '5%';
 
 }
 
 function inicio(){
+
     jogar = true;
     btnStart.style.visibility = 'hidden';
-    octodex.style.animation = 'jump 500ms ease-out';
     pipe.style.animation = 'whale-animation 1.5s infinite linear';
+    comeca();
     
 }
 
-const jump = () => {
-    octodex.classList.add('jump');
+function restart(){
 
+    jogar = true;
+
+    octodex.src = './images/octodex.gif'
+    btnRestart.style.visibility = 'hidden';
+    octodex.style.animation = 'jump 500ms ease-out';
+    octodex.style.bottom = '0';
+    octodex.style.width = '100px';
+
+    pipe.style.animation = 'whale-animation 1.5s infinite linear';
+    pipe.style.left= null;
+    
+    comeca();
+
+}
+
+const jump = () => {
+
+    octodex.classList.add('jump');
     setTimeout(() => {
         octodex.classList.remove('jump');
     },500)
 }
 
-const loop = setInterval(() => {
+function comeca(){
+    const loop = setInterval(() => {
 
-    const pipePosition = pipe.offsetLeft;
-    const octodexPosition = +window.getComputedStyle(octodex).bottom.replace('px', '');
+        const pipePosition = pipe.offsetLeft;
+        const octodexPosition = +window.getComputedStyle(octodex).bottom.replace('px', '');
+        
+        if(pipePosition <= 135 && pipePosition > 0 && octodexPosition < 80){
     
-    if(pipePosition <= 135 && pipePosition > 0 && octodexPosition < 80){
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`;
+    
+            octodex.style.animation = 'none';
+            octodex.style.bottom = `${octodexPosition}px`;
+    
+            octodex.src = './images/game-over.png';
+            octodex.style.width = '120px'
+    
+            btnRestart.style.visibility = 'initial';
+    
+            clearInterval(loop);
+        }
+    
+    },10);
+}
 
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
-
-        octodex.style.animation = 'none';
-        octodex.style.bottom = `${octodexPosition}px`;
-
-        octodex.src = './images/game-over.png';
-        octodex.style.width = '120px'
-
-        btnRestart.style.visibility = 'initial';
-
-        clearInterval(loop);
-    }
-
-},10);
 
 
 document.addEventListener('keydown', jump);
