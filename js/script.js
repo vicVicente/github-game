9434114a19d8fc5aae6caf31db69bbcf0237d13d
Victor Vicente <vicente.victor1507@gmail.com>
2023-04-14 23:38:49 -0300

    upload upgrades

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 const octodex = document.querySelector('.octodex');
 const pipe = document.querySelector('.whale');
+const btnStart = document.querySelector('#start');
+const btnRestart = document.querySelector('#restart');
+
+let jogar = false;
+
+
+if(jogar == false){
+
+    octodex.style.animation = 'none'
+    pipe.style.animation = 'none';
+    pipe.style.right = '5%';
+
+}
+
+function inicio(){
+    jogar = true;
+    btnStart.style.visibility = 'hidden';
+    octodex.style.animation = 'jump 500ms ease-out';
+    pipe.style.animation = 'whale-animation 1.5s infinite linear';
+    
+}
 
 const jump = () => {
     octodex.classList.add('jump');
@@ -13,8 +34,6 @@ const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
     const octodexPosition = +window.getComputedStyle(octodex).bottom.replace('px', '');
-
-    console.log(octodexPosition);
     
     if(pipePosition <= 135 && pipePosition > 0 && octodexPosition < 80){
 
@@ -27,11 +46,12 @@ const loop = setInterval(() => {
         octodex.src = './images/game-over.png';
         octodex.style.width = '120px'
 
+        btnRestart.style.visibility = 'initial';
+
         clearInterval(loop);
     }
 
-},10)
+},10);
 
 
 document.addEventListener('keydown', jump);
-
